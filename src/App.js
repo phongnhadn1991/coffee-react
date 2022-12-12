8b45d63c1d797d78b1587b51917001ef325d8504
ngoanmc <phongnhadn1991@gmail.com>
2022-12-12 23:26:28 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Footer from './layouts/footer/Footer';
-import Header from './layouts/header/Header'
+import Header from './layouts/header/Header';
 import RouterPage from './router/RouterPage';
 function App() {
   return (
