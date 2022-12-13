e59c57528ed1c05a17ccf79d8408fcf8910050a1
NgoanMC <ngoanmc@concrete-corp.com>
2022-12-13 10:53:36 +0700

    update info Login Ui

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -67,25 +67,28 @@ const LoginPage = (props) => {
                                                     <button onClick={handleGoogle} type="button" className="btn btn-primary btn-floating mx-1">
                                                         <i className="bi bi-google" />
                                                     </button>
-                                                    <button type="button" className="btn btn-primary btn-floating mx-1">
+                                                    <button type="button" className="btn btn-primary btn-floating mx-1" disabled>
                                                         <i className="bi bi-facebook" />
                                                     </button>
                                                 </div>
-                                                <div className="divider d-flex align-items-center my-4">
-                                                    <p className="text-center fw-bold mx-3 mb-0">Hoặc</p>
+                                                <div className='my-4'>
+                                                    <Alert message="Chỉ đăng nhập với google" type="info" showIcon />
                                                 </div>
+                                                {/* <div className="divider d-flex align-items-center my-4">
+                                                    <p className="text-center fw-bold mx-3 mb-0">Hoặc</p>
+                                                </div> */}
                                                 {/* Email input */}
                                                 <div className="form-outline mb-4">
-                                                    <input type="email" id="form3Example3" className="form-control form-control-md" placeholder="Tên đăng nhập" />
+                                                    <input type="email" id="form3Example3" className="form-control form-control-md" placeholder="Tên đăng nhập" disabled />
                                                 </div>
                                                 {/* Password input */}
                                                 <div className="form-outline mb-3">
-                                                    <input type="password" id="form3Example4" className="form-control form-control-md display-3" placeholder="Mật khẩu" />
+                                                    <input type="password" id="form3Example4" className="form-control form-control-md display-3" placeholder="Mật khẩu" disabled />
                                                 </div>
                                                 <div className="d-flex justify-content-between align-items-center">
                                                     {/* Checkbox */}
                                                     <div className="form-check mb-0">
-                                                        <input className="form-check-input me-2" type="checkbox" defaultValue id="form2Example3" />
+                                                        <input className="form-check-input me-2" type="checkbox" defaultValue id="form2Example3" disabled/>
                                                         <label className="form-check-label" htmlFor="form2Example3">
                                                             Lưu đăng nhập
                                                         </label>
@@ -93,7 +96,7 @@ const LoginPage = (props) => {
                                                     <a href="#!" className="text-body">Quên mật khẩu?</a>
                                                 </div>
                                                 <div className="text-center text-lg-start mt-4 pt-2">
-                                                    <button type="button" className="btn btn-primary btn-md" style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}>Đăng nhập</button>
+                                                    <button type="button" className="btn btn-primary btn-md" style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }} disabled>Đăng nhập</button>
                                                     <p className="small fw-bold mt-2 pt-1 mb-0">Nếu chưa có tài khoản? <a href="#!" className="link-danger">Tạo tài khoản mới</a></p>
                                                 </div>
                                             </form>
